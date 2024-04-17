07352b6af171cc463b61eb98b5439f3dbd5a6fc8
Bo Carey <38997775+bo-carey@users.noreply.github.com>
2024-04-17 13:26:28 -0400

    Add static recommendation data

--- src/constants.ts
+++ src/constants.ts
@@ -2,3 +2,46 @@ export const RESUME_URI =
   'https://drive.google.com/file/d/1TcDfGZGf4zGXEahZWcl21ZLr8y6vyV9g/view?usp=sharing';
 
 export const GET_REPOS_URI = 'https://api.github.com/users/bo-carey/repos';
+
+const RECOMMENDATIONS = [
+  {
+    "name": "Bryant Worcester",
+    "title": "Software Engineer II",
+    "company": "VinSolutions",
+    "linkedin": "https://www.linkedin.com/in/bryant-worcester-8a6b44155/",
+    "recommendation": "I worked with Bo for a year, and give him my full recommendation. Bo is excellent at communication. He is swift to give/respond to feedback and comments on reviews, resourceful at solving unique challenges, and provides well written reports when writing documentation. If you give Bo an issue, you can rest easy knowing that he will fully investigate it and return with options on how it can be handled.",
+    "date": "July 12, 2022"
+  },
+  {
+    "name": "Surabhi Khachar",
+    "title": "Software Engineer",
+    "company": "C2FO",
+    "linkedin": "https://www.linkedin.com/in/surabhikhachar/",
+    "recommendation": "Bo was a pleasure to work with during our time together at C2FO! Always willing to help with operational issues and would leave no stone unturned when tackling complex, layered technical tasks. He is resourceful and is constantly looking for ways to improve code bases. He is a quick learner and asks good 'why' questions. He'll never say no to picking up tasks/projects that are beyond his ability, and will spend time making sure he thoroughly understands the problem at hand before architecting a solution, all with a positive attitude and a smile on his face (no matter how tough the situation).",
+    "date": "July 6, 2022"
+  },
+  {
+    "name": "Clif Stubbs",
+    "title": "Software ",
+    "company": "IQVIA",
+    "linkedin": "https://www.linkedin.com/in/clif-stubbs",
+    "recommendation": "Bo and I worked together for about a year at Crispin and he was a pleasure to work with. He was very dedicated to learning and applying what he knew to deliver refined user interfaces. He is a capable individual and solid team member that I could depend on. ",
+    "date": "March 22, 2021"
+  },
+  {
+    "name": "Brandon Southwick",
+    "title": "General Supervisor",
+    "company": "Mitsubishi Electric",
+    "linkedin": "https://www.linkedin.com/in/brandon-southwick-25775813",
+    "recommendation": "I recommend hiring and promoting Bo for opportunities where he applies. Bo demonstrates the ability to work with others, work with limited supervision, use time wisely, and solve problems effectively. Bo is always positive and motivated.",
+    "date": "April 25, 2020"
+  },
+  {
+    "name": "Mark Philipp",
+    "title": "Software Engineer",
+    "company": "NVIDIA",
+    "linkedin": "https://www.linkedin.com/in/markallenphilipp",
+    "recommendation": "I worked alongside Bo at Epoch Games. For the short time I was associated with Epoch Games, I was struck by Bo's communication. Epoch Games was an organization where everyone worked remotely. We all communicated through programs such as Slack and Skype. Bo was always engaged in team meetings and asked clarifying questions to ensure development goals were properly met. He would also help brainstorm solutions to technical challenges. Having worked remotely for almost 10 years, I can't stress how important it is to be in regular communication with your team and ensure you have a clear understanding of your team's objectives. Bo understood this. Bo was also persistent about maintaining his assigned cards on our Trello board. He would provide well-written updates and clear information about any problems he was facing. Given the remote nature of Epoch Games, this is also critical to ensure all members of the company are aware of the progress being made and where outstanding issues lie. I also appreciated Bo's willingness to help other team members. When somebody had a question or an issue with something, he was often engaged in helping them find a solution. He would also do so in a supportive way. He was never condescending or short with anyone else. He certainly helped me get the Unreal Engine source code compiled and running. He is absolutely an enthusiastic team player and a great programmer.",
+    "date": "August 24, 2019"
+  }
+]
